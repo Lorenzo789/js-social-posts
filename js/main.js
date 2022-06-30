aebe444e563a6dbd16649ef1f49d17d5b2c4a9ca
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-30 14:57:20 +0200

    test

--- js/main.js
+++ js/main.js
@@ -132,4 +132,62 @@ const posts = [
 // Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
 
 
+// ! FUNCTION ! \\
+function parentWrapper(element) {
 
+    const wrapper = document.getElementById('container');
+
+    wrapper.append(element);
+
+    return wrapper;
+
+}
+// ! FUNCTION ! \\
+
+// ! FUNCTION ! \\
+function createElement(element) {
+    
+    const newElement = document.createElement('element');
+
+    newElement.innerHTML =
+    `<div class="post">
+    <div class="post__header">
+        <div class="post-meta">
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${posts.author.image}" alt="${posts.author.name}">
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${posts.author.name}</div>
+                <div class="post-meta__time">${posts.created}</div>
+            </div>
+        </div>
+    </div>
+    <div class="post__text">${posts.content}</div>
+    <div class="post__image">
+        <img src="${posts.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+            </div>
+        </div>
+    </div>
+</div>`
+
+return newElement;
+
+}
+
+posts.forEach( (element, index) => {
+
+    const newContainer = createElement(element);
+
+    parentWrapper(newContainer);
+});
